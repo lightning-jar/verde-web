af1da66d8b76c7e942fb1b38b0f72e16d48d8d0f
kevinpeckham <3825256+kevinpeckham@users.noreply.github.com>
2024-05-01 09:31:52 -0400

    style: tweak to error page

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -18,13 +18,13 @@ Contact Page
 		name="robots" />
 </svelte:head>
 <template lang="pug">
-	.bg-gradient-to-b.from-penn.via-penn-725.to-penn.pt-28.page-x-padding.mb-24
+	.bg-gradient-to-b.from-penn.via-penn-725.to-penn.pt-28.page-x-padding.mb-20
 		//- Header
 		.text-36.max-w-md.leading-snug.mb-6 404 Error
 		WidgetText(
 			classes="block !text-left pb-2") Not Found. The page you requested could not be found.
 		//- sec 1
-	.border-t.page-x-padding.pt-8.mb-16.grid.gap-x-12.pb-24.pt-0.grid.grid-cols-1.place-content-start(
+	.page-x-padding.pt-8.mb-16.grid.gap-x-12.pb-24.pt-0.grid.grid-cols-1.place-content-start(
 		class="lg:grid-cols-2 border-neutral-100/40")
 		a.opacity-90.underline.underline-offset-4(
 			class="decoration-white/40 hover:decoration-white transition-colors",
